2acf400bd720a2d4c4f107c5badbd60f49ae05e0
Kravchuk <majerkravchuk@gmail.com>
2018-08-05 00:13:34 +0300

    Add Buefy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+
 import './registerServiceWorker';
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
