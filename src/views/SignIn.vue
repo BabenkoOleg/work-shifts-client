5af14f852f4179703daff5d63e8d5033a6ad471b
Kravchuk <majerkravchuk@gmail.com>
2018-08-05 00:20:36 +0300

    Add template for SignIn page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="sign-in">
+    <form class="sign-in" @submit.prevent="login">
+      <transition name="fade">
+        <h3 v-if="showSiteName" class="subtitle">Work Shifts</h3>
+      </transition>
+      <b-field>
+        <b-input required v-model="email" placeholder="Email" :type="email" icon="email">
+        </b-input>
+      </b-field>
+      <b-field>
+        <b-input v-model="password" placeholder="Password" type="password" icon="lock" required>
+        </b-input>
+      </b-field>
+      <b-notification v-if="invalid" type="is-danger" :closable="false">
+        Email/Password is incorrect
+      </b-notification>
+      <b-field>
+        <button class="button is-success is-fullwidth">
+          Sign In
+        </button>
+      </b-field>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showSiteName: false,
+      email: '',
+      password: '',
+      invalid: false,
+    };
+  },
+
+  created() {
+    document.body.classList.add('sign-in');
+  },
+
+  mounted() {
+    this.showSiteName = true;
+  },
+
+  destroyed() {
+    document.body.classList.remove('sign-in');
+  },
+
+  methods: {
+    login() {
+      console.log('sending email and password');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: all 2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: translateX(20px);
+  }
+
+  .sign-in {
+    left: 50%;
+    max-width: 400px;
+    padding: 30px;
+    position: absolute;
+    transform: translate(-50%, calc(-50% - 10px));
+    top: 50%;
+    width: 100%;
+
+    .subtitle {
+      color: #fff;
+      font-size: 35px;
+      margin-bottom: 10px;
+      text-align: center;
+    }
+
+    /deep/ .notification {
+      margin-bottom: 12px;
+      padding: 1rem 1.25rem 1rem 1.25rem;
+
+      .media {
+        text-align: center;
+
+        .media-content {
+          font-size: 1rem;
+          text-align: center;
+        }
+      }
+    }
+  }
+</style>
