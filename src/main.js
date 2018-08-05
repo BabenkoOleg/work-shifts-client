3be0ae77385248d46a35e4beb3207922bfb632fa
Kravchuk <majerkravchuk@gmail.com>
2018-08-05 14:41:20 +0300

    Remove WPA

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import './registerServiceWorker';
-
 Vue.use(Buefy);
 Vue.config.productionTip = false;
 
