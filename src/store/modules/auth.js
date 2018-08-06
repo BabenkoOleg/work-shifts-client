091f244dab3001cd7c9e8b66c1e608ed372a07cd
Kravchuk <majerkravchuk@gmail.com>
2018-08-07 02:19:13 +0300

    Implement sign out

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,11 +3,13 @@ import axiosInstance from '@/store/axios-instance';
 
 export const endpoints = {
   SIGN_IN: '/auth/sign-in',
+  SIGN_OUT: '/auth/sign-out',
   CURRENT_USER: '/auth/current-user',
 };
 
 export const actionTypes = {
   SIGN_IN: 'SIGN_IN',
+  SIGN_OUT: 'SIGN_OUT',
   GET_CURRENT_USER: 'GET_CURRENT_USER',
 };
 
@@ -22,6 +24,10 @@ export default {
     currentUser: null,
   },
 
+  getters: {
+    currentUser: state => state.currentUser,
+  },
+
   actions: {
     [actionTypes.SIGN_IN]({ commit }, { email, password }) {
       return axiosInstance.post(endpoints.SIGN_IN, { user: { email, password } })
@@ -29,6 +35,11 @@ export default {
           const currentUser = dataFormatter.deserialize(data);
           commit(mutationTypes.SET_CURRENT_USER, currentUser);
         });
+    },
+
+    [actionTypes.SIGN_OUT]({ commit }) {
+      return axiosInstance.delete(endpoints.SIGN_OUT)
+        .then(() => commit(mutationTypes.SET_CURRENT_USER, null));
     },
 
     [actionTypes.GET_CURRENT_USER]({ commit }) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="home">
+    <div v-if="currentUser" class="hello">
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+          Hello, {{ currentUser.name }}!
+        </div>
+      </div>
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+          <button class="button is-danger" @click="signOut">Sign Out</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+import { actionTypes as authActionTypes } from '@/store/modules/auth';
+
 export default {
   name: 'home',
+  computed: {
+    ...mapGetters('auth', ['currentUser']),
+  },
+
+  methods: {
+    ...mapActions('auth', [authActionTypes.SIGN_OUT]),
+
+    signOut() {
+      this[authActionTypes.SIGN_OUT]().then(() => {
+        this.$router.push({ name: 'signIn' });
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.hello {
+  margin-top: 120px;
+  text-align: center;
+}
 </style>
