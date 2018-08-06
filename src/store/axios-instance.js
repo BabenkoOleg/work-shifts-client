da99bf3e1fbf2557946f3e9703ff258db044056f
Kravchuk <majerkravchuk@gmail.com>
2018-08-07 01:24:54 +0300

    Add Snackbar for 5** errors

--- src/store/axios-instance.js
+++ src/store/axios-instance.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Snackbar } from 'buefy';
 
 const axiosInstance = axios.create({
   baseURL: '/api/',
@@ -10,6 +11,27 @@ axiosInstance.interceptors.request.use((config) => {
     config.headers['Http-Authorization'] = `Bearer ${jwt}`;
   }
   return config;
+});
+
+axiosInstance.interceptors.response.use(() => {}, (error) => {
+  if (error.response.status >= 500) {
+    let errorMessage;
+
+    if (process.env.NODE_ENV === 'production') {
+      errorMessage = 'Something went wrong, but we are already fixing it!';
+    } else {
+      errorMessage = `Server error: ${error.response.statusText} [${error.response.status}]`;
+    }
+
+    Snackbar.open({
+      duration: 5000,
+      message: errorMessage,
+      type: 'is-danger',
+      position: 'is-bottom',
+      actionText: 'Ok',
+      queue: false,
+    });
+  }
 });
 
 export default axiosInstance;
