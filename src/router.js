c9a23efed109ba2629fc08e4da9bc9412a926153
Kravchuk <majerkravchuk@gmail.com>
2018-08-09 00:19:32 +0300

    Add an authorization check when navigating the site

--- src/router.js
+++ src/router.js
@@ -4,7 +4,25 @@ import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import SignIn from '@/views/SignIn.vue';
 
+import store from './store';
+
 Vue.use(Router);
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+  } else {
+    next({ name: 'signIn' });
+  }
+};
 
 export default new Router({
   mode: 'history',
@@ -13,11 +31,13 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      beforeEnter: ifAuthenticated,
     },
     {
       path: '/sign-in',
       name: 'signIn',
       component: SignIn,
+      beforeEnter: ifNotAuthenticated,
     },
   ],
 });
