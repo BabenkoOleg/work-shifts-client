6bc25e3d47c2052b6e2bec8af2ccbc7b680f1ad9
Kravchuk <majerkravchuk@gmail.com>
2018-08-17 21:04:09 +0300

    Fix header layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,31 +2,39 @@
   <div class="header">
     <nav class="navbar">
       <div class="navbar-brand">
-        <router-link class="navbar-item" :to="{ name: 'home' }">
+        <router-link class="navbar-item navbar-item-logo" :to="{ name: 'home' }">
           WorkShifts | {{currentUser.business.name}}
         </router-link>
+        <a role="button"
+           class="navbar-burger"
+           :class="{ 'is-active': isMenuActive }"
+           aria-label="menu"
+           aria-expanded="false"
+           @click="toggleMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div class="navbar-menu">
+      <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
         <div class="navbar-end">
-          <b-dropdown position="is-bottom-left">
-            <a class="navbar-item" slot="trigger">
-              <span class="user-name">{{currentUser.name || currentUser.email}}</span>
-              <b-icon icon="account"></b-icon>
-            </a>
-            <b-dropdown-item v-if="currentUser.role === 'administrator'" @click="goToAdminPanel">
-              <b-icon icon="account-settings-variant"></b-icon>
-              <span class="dropdown-item-label">Admin Panel</span>
-            </b-dropdown-item>
-            <hr class="dropdown-divider">
-            <b-dropdown-item disabled>
-              <b-icon icon="settings"></b-icon>
-              <span class="dropdown-item-label">Settings</span>
-            </b-dropdown-item>
-            <b-dropdown-item @click="signOut">
-              <b-icon icon="logout"></b-icon>
-              <span class="dropdown-item-label">Sign Out</span>
-            </b-dropdown-item>
-          </b-dropdown>
+          <navbar-item class="navbar-item user-name">
+            <span class="navbar-item-label">{{currentUser.name || currentUser.email}}</span>
+            <b-icon icon="account"></b-icon>
+          </navbar-item>
+          <hr>
+          <navbar-item class="navbar-item">
+            <span class="navbar-item-label">Admin Panel</span>
+            <b-icon icon="account-settings-variant"></b-icon>
+          </navbar-item>
+          <navbar-item class="navbar-item">
+            <span class="navbar-item-label">Settings</span>
+            <b-icon icon="settings"></b-icon>
+          </navbar-item>
+          <navbar-item class="navbar-item">
+            <span class="navbar-item-label">Sign Out</span>
+            <b-icon icon="logout"></b-icon>
+          </navbar-item>
         </div>
       </div>
     </nav>
@@ -38,12 +46,22 @@ import { mapActions, mapState } from 'vuex';
 import { actionTypes as authActionTypes } from '@/store/modules/auth';
 
 export default {
+  data() {
+    return {
+      isMenuActive: false,
+    };
+  },
+
   computed: {
     ...mapState('auth', ['currentUser']),
   },
 
   methods: {
     ...mapActions('auth', [authActionTypes.SIGN_OUT]),
+
+    toggleMenu() {
+      this.isMenuActive = !this.isMenuActive;
+    },
 
     signOut() {
       this[authActionTypes.SIGN_OUT]().then(() => {
@@ -67,29 +85,47 @@ export default {
     padding-left: 10px;
     padding-right: 10px;
 
-    .navbar-item {
-      background-color: #3d4852;
-      color: #fff;
+    .navbar-brand {
+      .navbar-item-logo {
+        color: #dedede;
+        font-size: 18px;
 
-      .user-name {
-        margin-right: 10px;
+        &:hover {
+          color: #fff;
+          cursor: pointer;
+        }
       }
 
-      &:hover {
-        background-color: #343a40;
+      .navbar-burger {
+        color: #dedede;
       }
     }
 
-    .dropdown-menu {
-      .dropdown-item {
-        display: flex;
+    .navbar-menu {
+      .navbar-end {
+        .navbar-item {
+          background-color: #3d4852;
+          color: #dedede;
+          display: flex;
+          font-size: 14px;
 
-        .dropdown-item-label {
-          line-height: 24px;
-        }
+          &:hover:not(.user-name) {
+            background-color: #343a40;
+            color: #fff;
+            cursor: pointer;
+          }
 
-        .icon {
-          margin-right: 8px;
+          hr {
+            display: none;
+          }
+
+          .navbar-item-label {
+            line-height: 24px;
+          }
+
+          .icon {
+            margin-left: 8px;
+          }
         }
       }
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,13 @@
 <template>
   <div class="home">
-    <div v-if="currentUser" class="hello">
-      <div class="columns is-mobile is-centered">
-        <div class="column is-half">
-          Hello, {{ currentUser.name }}!
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
 export default {
   name: 'home',
-
-  computed: {
-    ...mapState('auth', ['currentUser']),
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-.hello {
-  margin-top: 120px;
-  text-align: center;
-}
 </style>
