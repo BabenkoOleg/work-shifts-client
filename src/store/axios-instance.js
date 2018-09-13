bf175d18c3ae8fb9e3be2598cc889e2f46090834
Kravchuk <majerkravchuk@gmail.com>
2018-09-13 20:09:31 +0300

    Remove installing jwt on requests to the server

--- src/store/axios-instance.js
+++ src/store/axios-instance.js
@@ -6,14 +6,6 @@ const axiosInstance = axios.create({
   baseURL: '/api/',
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const jwt = window.localStorage.getItem('jwt');
-  if (jwt) {
-    config.headers['Http-Authorization'] = `Bearer ${jwt}`;
-  }
-  return config;
-});
-
 axiosInstance.interceptors.response.use((response) => {
   return Promise.revolve(dataFormatter.deserialize(response.data));
 }, (error) => {
