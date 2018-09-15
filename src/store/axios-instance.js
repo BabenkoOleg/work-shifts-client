b8951ccc0980eff87908add7abb55935152fa0b4
Kravchuk <majerkravchuk@gmail.com>
2018-09-15 18:32:37 +0300

    Use callbacks in the axios-instance

--- src/store/axios-instance.js
+++ src/store/axios-instance.js
@@ -6,35 +6,41 @@ const axiosInstance = axios.create({
   baseURL: '/api/',
 });
 
-axiosInstance.interceptors.response.use((response) => {
-  return Promise.revolve(dataFormatter.deserialize(response.data));
-}, (error) => {
+const onFulfilled = (response) => {
+  if (response.data) return Promise.resolve(dataFormatter.deserialize(response.data));
+  return null;
+};
+
+const onRejected = (error) => {
   if (error.response.status < 500) {
-    return Promise.reject(dataFormatter.deserialize(error.response.data));
-  } else {
-    let errorMessage;
-
-    if (process.env.NODE_ENV === 'production') {
-      errorMessage = 'Something went wrong, but we are already fixing it!';
-    } else {
-      errorMessage = `Server error: ${error.response.statusText} [${error.response.status}]`;
-    }
-
-    Snackbar.open({
-      duration: 5000,
-      message: errorMessage,
-      type: 'is-danger',
-      position: 'is-bottom',
-      actionText: 'Ok',
-      queue: false,
-    });
-
-    error.response.data = {
-      error: `${error.response.statusText} [${error.response.status}]`,
-    };
+    if (error.response.data) return Promise.reject(dataFormatter.deserialize(error.response.data));
+    return null;
   }
 
+  let errorMessage = null;
+
+  if (process.env.NODE_ENV === 'production') {
+    errorMessage = 'Something went wrong, but we are already fixing it!';
+  } else {
+    errorMessage = `Server error: ${error.response.statusText} [${error.response.status}]`;
+  }
+
+  Snackbar.open({
+    duration: 5000,
+    message: errorMessage,
+    type: 'is-danger',
+    position: 'is-bottom',
+    actionText: 'Ok',
+    queue: false,
+  });
+
+  error.response.data = {
+    error: `${error.response.statusText} [${error.response.status}]`,
+  };
+
   return Promise.reject(error);
-});
+};
+
+axiosInstance.interceptors.response.use(onFulfilled, onRejected);
 
 export default axiosInstance;
